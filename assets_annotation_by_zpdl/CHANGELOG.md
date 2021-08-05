bd7c92a2ecc7af75d85b7976ddd1182444255c49
김경환 <zpdl9089@storecamera.io>
2021-08-05 13:00:47 +0900

    assets_annotation_by_zpdl : 1.0.2

--- assets_annotation_by_zpdl/CHANGELOG.md
+++ assets_annotation_by_zpdl/CHANGELOG.md
@@ -1,3 +1,7 @@
+## 1.0.2
+
+* add isCamelCase & isSnakeCase
+
 ## 1.0.1
 
 * update README and description
--- assets_annotation_by_zpdl/lib/assets_annotation_by_zpdl.dart
+++ assets_annotation_by_zpdl/lib/assets_annotation_by_zpdl.dart
@@ -1,7 +1,19 @@
 library assets_annotation_by_zpdl;
 
 const assetsAnnotation = AssetsAnnotation();
+const assetsAnnotationCamel = AssetsAnnotation(isCamelCase: true);
+const assetsAnnotationSnake = AssetsAnnotation(isSnakeCase: true);
 
+/// Annotation for assets_generator_by_zpdl package
 class AssetsAnnotation {
-  const AssetsAnnotation();
+  /// Camel case enable value
+  /// The first letter is written in lowercase. The first letter of the rest of the following words is capitalized.
+  /// ex) isCamelCase
+  final bool isCamelCase;
+  /// Snake case enable value
+  /// Place an underline in the middle of each word joined.
+  /// ex) is_snake_case
+  final bool isSnakeCase;
+
+  const AssetsAnnotation({this.isCamelCase = false, this.isSnakeCase = false});
 }
